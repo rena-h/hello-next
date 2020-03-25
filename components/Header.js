1db12faa216cb84007b123022c82b3f91859c9b1
Rena Hamada <renahamada0221@gmail.com>
2020-03-25 23:29:27 +0900

    feat: create header component

---
+++ components/Header.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+const linkStyle = {
+  marginRight: 15
+};
+
+const Header = () => (
+  <div>
+    <Link href="/">
+      <a style={linkStyle} title="Home">
+        Home
+      </a>
+    </Link>
+    <Link href="/about" as="/about/hoge">
+      <a style={linkStyle} title="About Page">
+        About
+      </a>
+    </Link>
+  </div>
+);
+
+export default Header;
